87b7258b0bd95f8649c649d4f3263d8451fe5512
Chinmay <chinmaybarik214@gmail.com>
2024-02-11 18:30:23 +0530

    changed emoji to icon

--- omcosmetics/src/App.js
+++ omcosmetics/src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p className="fire-icon" alt="Fire emoji">🔥</p>
+      <p className="fire-icon" alt="Fire emoji">
+        <i class="fa-solid fa-fire"></i>
+      </p>
       <h1>Om Cosmetics</h1>
       <p>This is where the website for Om cosmetics is deployed via firebase</p>
     </div>
